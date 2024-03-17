c4737a25bd8c7b952c6682d5b1317b1a6dd5ed34
orchingly <orchingly@gmail.com>
2024-03-17 10:56:15 +0800

    修改域名，端口和默认logo

--- vue_mogu_admin/config/dev.env.js
+++ vue_mogu_admin/config/dev.env.js
@@ -6,13 +6,13 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
 
   //开发环境
-  ADMIN_API: '"http://localhost:8607/mogu-admin"',
-  PICTURE_API: '"http://localhost:8607/mogu-picture"',
-  WEB_API: '"http://localhost:8607/mogu-web"',
-  Search_API: '"http://localhost:8607/mogu-search"',
+  ADMIN_API: '"https://dailys.top:8888/mogu-admin"',
+  PICTURE_API: '"https://dailys.top:8888/mogu-picture"',
+  WEB_API: '"https://dailys.top:8888/mogu-web"',
+  Search_API: '"https://dailys.top:8888/mogu-search"',
   Spider_API: '"http://localhost:8607/mogu-spider"',
   FILE_API: '"http://localhost:8600/"',
-  BLOG_WEB_URL: '"http://localhost:9527"',
+  BLOG_WEB_URL: '"https://dailys.top:8880"',
   SOLR_API: '"http://localhost:8080/solr"',
   ELASTIC_SEARCH: '"http://localhost:5601"',
 })
