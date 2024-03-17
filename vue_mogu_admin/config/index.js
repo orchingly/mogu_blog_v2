c3bb9ab339b5e3ba9da5e7b0a4e85e9231005399
orchingly <orchingly@gmail.com>
2024-03-17 10:53:30 +0800

    node 监听为ip 0.0.0.0, 可以外部访问调试

--- vue_mogu_admin/config/index.js
+++ vue_mogu_admin/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '0.0.0.0', // can be overwritten by process.env.HOST
     port: 9528, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
@@ -22,7 +22,7 @@ module.exports = {
     // Use Eslint Loader?
     // If true, your code will be linted during bundling and
     // linting errors and warnings will be shown in the console.
-    useEslint: true,
+    useEslint: false,
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
     showEslintErrorsInOverlay: false,
@@ -33,14 +33,14 @@ module.exports = {
 
     // https://webpack.js.org/configuration/devtool/#development
     // devtool: 'cheap-source-map',
-    devtool: 'source-map',
+    devtool: 'cheap-module-eval-source-map',
 
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
-    cssSourceMap: false
+    cssSourceMap: false,
   },
 
   build: {
