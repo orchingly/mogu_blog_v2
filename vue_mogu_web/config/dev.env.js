39095eaa328b2029e5ca2ea32d94b9b7b98301f7
orchingly <orchingly@gmail.com>
2024-03-17 10:59:08 +0800

    调整网站域名，HOST 0.0.0.0 外部可访问调试

--- vue_mogu_web/config/dev.env.js
+++ vue_mogu_web/config/dev.env.js
@@ -5,9 +5,9 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
 
-  VUE_MOGU_WEB: '"http://localhost:9527"',
-  PICTURE_API: '"http://localhost:8607/mogu-picture"',
-	WEB_API: '"http://localhost:8607/mogu-web"',
-  SEARCH_API: '"http://localhost:8607/mogu-search"',
+  VUE_MOGU_WEB: '"http://dailys.top:8880"',
+  PICTURE_API: '"https://dailys.top:8888/mogu-picture"',
+	WEB_API: '"https://dailys.top:8888/mogu-web"',
+  SEARCH_API: '"https://dailys.top:8888/mogu-search"',
 
 })
