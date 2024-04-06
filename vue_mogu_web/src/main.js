06deec86cc50e2d5aa9afaad89756dc310065e26
orchingly <orchingly@gmail.com>
2024-04-06 17:16:19 +0800

    Revert "添加行号测试代码，效果不理想，暂不添加"
    This reverts commit 551b0448c009017983aacbc8084f751c0c822be8.

--- vue_mogu_web/src/main.js
+++ vue_mogu_web/src/main.js
@@ -32,58 +32,11 @@ import MathJax from './utils/MathJax'
 Vue.prototype.MathJax = MathJax;
 
 import MetaInfo from 'vue-meta'
-import hljs from '../static/cdn/highlight';
-
-// import '../static/cdn/highlightjs-line-numbers.min.js'
-
-// const hljs = require('../static/cdn/highlight.js')
-
-// export async function highlightJSLoad () {
-//   await import('../static/cdn/highlight.js')
-// }
-// export async function highlightLineNumbersLoad () {
-//   await import('../static/cdn/highlightjs-line-numbers.min.js')
-// }
-
 Vue.use(MetaInfo)
 
 Vue.directive('highlight', function (el) {
-  let blocks = el.querySelectorAll('pre code')
-//   highlightJSLoad().then(() => {
-//     window.hljs = hljs
-
-//     highlightLineNumbersLoad().then(() => {
-//       hljs.initLineNumbersOnLoad()
-//       hljs.initLineNumbersOnLoad({
-//         singleLine: true
-//     })
-//       blocks.forEach((block) => {
-//         hljs.highlightBlock(block)
-//         hljs.lineNumbersBlock(block)
-//       })
-//     })
-//   })
-// })
-
+  let blocks = el.querySelectorAll('pre code');
   blocks.forEach((block) => {
-
-// 1.创建ul节点
-// let ul = document.createElement("ul");
-// // 2.根据换行符获取行数，根据获取的行数生成行号
-// let rowCount = block.outerHTML.split('\n').length;
-// for(let i=1;i <= rowCount - 1; i++){
-//   //创建li节点，创建文本节点
-//   let li = document.createElement("li")
-//   let text = document.createTextNode(i)
-//   //为li追加文本节点，将li加入ul
-//   li.appendChild(text)
-//   ul.appendChild(li)
-// }
-// // 3.给行号添加类名
-// ul.className = 'pre-numbering'
-// // 4.将ul节点加到 代码块
-// block.parentNode.appendChild(ul)
-
     hljs.highlightBlock(block)
   })
 })
